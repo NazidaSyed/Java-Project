a4e2aa787c914a4d30921c853a2013f49fe2ff7c
nazidasyed <syeda.nazida@gmail.com>
2022-10-08 21:37:40 -0400

    first commit

---
+++ src/java_basic_assignment/My_First_Assignment.java
@@ -0,0 +1,50 @@
+package java_basic_assignment;
+
+public class My_First_Assignment {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+   System.out.println("i am woman 1");
+   System.out.println("i am fearless 2");
+   System.out.println("i am sexy 3");
+   System.out.println("i am devine 4");
+   System.out.println("i am unbeatable 5");
+   System.out.println("i am creative 6");
+   System.out.println("i am anything i want 7");
+   System.out.println("i am woman 8");
+   System.out.println("i am woman 9");
+   System.out.println("i am woman 10");
+   System.out.println("i am woman 11");
+   System.out.println("i am woman 12");
+   System.out.println("i am woman 13");
+   System.out.println("i am woman 14");
+   System.out.println("i am woman 15");
+   System.out.println("i am woman 16");
+   System.out.println("i am woman 17");
+   System.out.println("i am woman 18");
+   System.out.println("i am woman 19");
+   System.out.println("i am woman 20");
+   
+   System.out.print("i am fearless 1");
+   System.out.print("i am fearless 2");
+   System.out.print("i am fearless 3");
+   System.out.print("i am fearless 4");
+   System.out.print("i am fearless 5");
+   System.out.print("i am fearless 6");
+   System.out.print("i am fearless 7");
+   System.out.print("i am fearless 8");
+   System.out.print("i am fearless 9");
+   System.out.print("i am fearless 10");
+   System.out.print("i am fearless 11");
+   System.out.print("i am fearless 12");
+   System.out.print("i am fearless 13");
+   System.out.print("i am fearless 14");
+   System.out.print("i am fearless 15");
+   System.out.print("i am fearless 16");
+   System.out.print("i am fearless 17");
+   System.out.print("i am fearless 18");
+   System.out.print("i am fearless 19");
+   System.out.print("i am fearless 20");
+	}
+
+}
---
+++ src/java_basic_assignment/My_First_Assignment1.java
@@ -0,0 +1,101 @@
+package java_basic_assignment;
+
+public class My_First_Assignment1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		//int
+		int a = 1;
+		int b = 2;
+		int c = 3;
+		int d = 4;
+		int e = 5;
+		int f = 6;
+		int g = 7;
+		int h = 8;
+		int i = 9;
+		int j = 10;
+		
+		System.out.println(a);
+		System.out.println(b);
+		System.out.println(c);
+		System.out.println(d);
+		System.out.println(e);
+		System.out.println(f);
+		System.out.println(g);
+		System.out.println(h);
+		System.out.println(i);
+		System.out.println(j);
+		
+		//boolean
+		boolean c1 = 4<5;
+		boolean c2 = 5>6;
+		boolean c3 = 4==4;
+		boolean c4 = 4==5;
+		boolean c5 = 5!=6;
+		boolean c6 = 6!=6;
+		boolean c7 = 5==0.5;
+		boolean c8 = 5!=0.5;
+		boolean c9 = 1>0.1;
+		boolean c10 = 1<0.1;
+		
+		System.out.println(c1);
+		System.out.println(c2);
+		System.out.println(c3);
+		System.out.println(c4);
+		System.out.println(c5);
+		System.out.println(c6);
+		System.out.println(c7);
+		System.out.println(c8);
+		System.out.println(c9);
+		System.out.println(c10);
+
+		//char
+		char a1 = 'a';
+		char a2 = 'b';
+		char a3 = 'c';
+		char a4 = 'd';
+		char a5 = 'e';
+		char a6 = 'f';
+		char a7 = 'g';
+		char a8 = 'h';
+		char a9 = 'i';
+		char a10 = 'j';
+		
+		System.out.println(a1);
+		System.out.println(a2);
+		System.out.println(a3);
+		System.out.println(a4);
+		System.out.println(a5);
+		System.out.println(a6);
+		System.out.println(a7);
+		System.out.println(a8);
+		System.out.println(a9);
+		System.out.println(a10);
+		
+		//double
+		double b1 = 1.1;
+		double b2 = 1.2;
+		double b3 = 1.3;
+		double b4 = 1.4;
+		double b5 = 1.5;
+		double b6 = 1.6;
+		double b7 = 1.7;
+		double b8 = 1.8;
+		double b9 = 1.9;
+		double b10 = 2.0;
+		
+		System.out.println(b1);
+		System.out.println(b2);
+		System.out.println(b3);
+		System.out.println(b4);
+		System.out.println(b5);
+		System.out.println(b6);
+		System.out.println(b7);
+		System.out.println(b8);
+		System.out.println(b9);
+		System.out.println(b10);	
+		
+	}
+
+}
